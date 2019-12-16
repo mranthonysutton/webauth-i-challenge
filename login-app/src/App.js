cde95f8f27ac02e3c414b2b9d8ba68b5fb4610f7
Anthony <anthony@pop-os.localdomain>
2019-12-16 15:37:13 -0800

    Added basic navigation menu for easy nav between various routes

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import Login from "./components/Login";
-import ListUsers from "./components/ListUsers";
-import Register from "./components/Register";
-import "./App.css";
+import React from 'react';
+import {Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import ListUsers from './components/ListUsers';
+import Register from './components/Register';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path={"/"} component={Login} />
-      <Route exact path={"/users"} component={ListUsers} />
-      <Route path={"/register"} component={Register} />
+      <Route path={'/'} component={NavBar} />
+      <Route exact path={'/'} component={Login} />
+      <Route exact path={'/users'} component={ListUsers} />
+      <Route path={'/register'} component={Register} />
     </div>
   );
 }
