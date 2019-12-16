c1ea2aac6e21c08dbaef68eade595babccd80dde
Anthony <anthony@pop-os.localdomain>
2019-12-16 15:09:11 -0800

    Built out register component that allows registration from the react app

--- login-app/src/components/Register.jsx
+++ login-app/src/components/Register.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from "react";
-import { Button, Container, TextField } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import React, {useState} from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import {Button, Container, TextField} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    "& > *": {
-      margin: theme.spacing(1)
-    }
-  }
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
 }));
 
-const Register = () => {
+const Register = props => {
   const classes = useStyles();
   const [registerCredentials, setRegisterCredentials] = useState([]);
 
   const handleRegister = event => {
     event.preventDefault();
-    console.log(registerCredentials);
+    axiosWithAuth()
+      .post('api/register', registerCredentials)
+      .then(response => {
+        console.log(response.data);
+        props.history.push('/users');
+      })
+      .catch(error => console.log(error));
   };
 
   const handleChange = event => {
     setRegisterCredentials({
       ...registerCredentials,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
   return (
-    <Container maxWidth={"sm"}>
+    <Container maxWidth={'sm'}>
       <h1>Register</h1>
       <form onSubmit={handleRegister}>
         <TextField
           required
           autoFocus
           fullWidth
-          margin={"normal"}
-          label={"Username..."}
-          variant={"outlined"}
+          margin={'normal'}
+          label={'Username...'}
+          variant={'outlined'}
           type="text"
           name="username"
           placeholder="Username..."
@@ -46,9 +53,9 @@ const Register = () => {
           required
           autoFocus
           fullWidth
-          margin={"normal"}
-          label={"Password..."}
-          variant={"outlined"}
+          margin={'normal'}
+          label={'Password...'}
+          variant={'outlined'}
           type="password"
           name="password"
           placeholder="Password..."
@@ -57,22 +64,20 @@ const Register = () => {
         <div className={classes.root}>
           <Button
             fullWidth
-            size={"large"}
-            margin={"normal"}
-            variant={"contained"}
-            color={"primary"}
-            type="submit"
-          >
+            size={'large'}
+            margin={'normal'}
+            variant={'contained'}
+            color={'primary'}
+            type="submit">
             Register
           </Button>
           <Button
             fullWidth
-            size={"large"}
-            margin={"normal"}
-            variant={"contained"}
-            color={"secondary"}
-            href="/"
-          >
+            size={'large'}
+            margin={'normal'}
+            variant={'contained'}
+            color={'secondary'}
+            href="/">
             Login
           </Button>
         </div>
