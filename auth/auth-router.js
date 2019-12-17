448c8d1105c28a52bed55849768ff06dc3002373
Anthony Sutton <anthony.sutton95gmail.com>
2019-12-17 14:29:40 -0800

    added comments for better readability and also changed logout to check for a req.session.user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,13 +3,17 @@ const router = require('express').Router();
 const Users = require('../users/users-model');
 
 function validateUserCredentials(req, res, next) {
+  // Checks if the body is empty before sending back errors to the client
   if (Object.keys(req.body).length === 0) {
     res.status(400).json({error: 'No information was passed into the body.'});
   } else {
+    // start the more specific requests to provide better error handling
     if (!req.body.username) {
       res.status(400).json({error: 'Please provide a username.'});
     } else if (!req.body.password) {
       res.status(400).json({error: 'Please provide a password.'});
+
+      // If no errors are found, move onto the actual endpoint
     } else {
       next();
     }
@@ -19,8 +23,11 @@ function validateUserCredentials(req, res, next) {
 function userAlreadyExists(req, res, next) {
   let username = req.body.username;
 
+  // Searches the database for the username that was passed in
   Users.findUserBy({username})
     .then(response => {
+      // If we get a response, we know that a user with that unique username already exists so return an error.
+      // If no user is found, allow the endpoint to be accessed
       if (response) {
         res.status(400).json({error: 'That username already exists.'});
       } else {
@@ -41,11 +48,13 @@ router.post(
   (req, res) => {
     let user = req.body;
 
+    // hashes the password prior to sending it over to the client
     const hash = bcrypt.hashSync(user.password, 12);
     user.password = hash;
 
     Users.addUser(user)
       .then(response => {
+        // The response is set to return back the newly created user
         res.status(201).json(response);
       })
       .catch(error => {
@@ -61,6 +70,8 @@ router.post('/login', validateUserCredentials, (req, res) => {
   Users.findUserBy({username})
     .first()
     .then(user => {
+      // Need to grab the password of the username that was used to login, and check if the hashed password and the password the user provided match
+      // If the user matches, sets a session of the user object to allow access to restricted routes
       if (user && bcrypt.compareSync(password, user.password)) {
         // saves a session for the user credentials
         req.session.user = user;
@@ -79,7 +90,7 @@ router.post('/login', validateUserCredentials, (req, res) => {
 
 router.get('/logout', (req, res) => {
   // check if a sessions exists so that it can be destroyed
-  if (req.session) {
+  if (req.session.user) {
     // If their was an error while destroying the session, let the user no, if not, provide them with a success message
     req.session.destroy(error => {
       if (error) {
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,7 +1,8 @@
 module.exports = (req, res, next) => {
+  // checks if their is a valid session and a valid user
   if (req.session && req.session.user) {
     next();
   } else {
-    res.status(401).json({error: 'You do not have access.'});
+    res.status(401).json({ error: "You do not have access." });
   }
 };
